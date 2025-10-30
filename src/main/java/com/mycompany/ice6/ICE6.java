3d0f9e861b332c9898bf28f6b9ecaadbd7973087
Prajna Praveen <ST10470459@VCCONNECT.edu.za>
2025-10-30 08:27:50 +0200

    initial: first version of the project

---
+++ src/main/java/com/mycompany/ice6/ICE6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice6;
+
+/**
+ *
+ * @author prave
+ */
+public class ICE6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
